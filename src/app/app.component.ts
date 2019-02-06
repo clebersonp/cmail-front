eb4b390948c313567ffa1303884e25dabafaad26
Cleberson <pauluci.cleberson@gmail.com>
2019-02-05 22:50:40 -0200

    utilizando diretivas ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cmail-front';
+  private _isNewEmailFormOpen = false;
+
+  emails = [
+    {assunto : "Assunto 1", destinatario : "joao.silva@cmail.com", conteudo : "Conteúdo do email"},
+    {assunto : "Assunto 2", destinatario : "carlos.aleman@cmail.com", conteudo : "Conteúdo do email"},
+  ];
+  
+  toogleNewEmailForm() {
+    this._isNewEmailFormOpen = !this._isNewEmailFormOpen;
+  }
+
+// ctrl + D ou F2 ajuda na refatoracao do codigo
+
+  get isNewEmailFormOpen() {
+    return this._isNewEmailFormOpen;
+  }
 }
