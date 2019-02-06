78f66d180ef9d5dda30fdfdc09188704d35da501
Cleberson <pauluci.cleberson@gmail.com>
2019-02-06 21:33:23 -0200

    implementando two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,32 @@ export class AppComponent {
     {assunto : "Assunto 1", destinatario : "joao.silva@cmail.com", conteudo : "Conteúdo do email"},
     {assunto : "Assunto 2", destinatario : "carlos.aleman@cmail.com", conteudo : "Conteúdo do email"},
   ];
+
+  // email sera usado com o way 2 data binding
+  email = {
+    assunto : '',
+    destinatario : '',
+    conteudo : ''
+  };
+
+  handleCriarEmail(eventoDoForm) {
+    // mudar o comportamento default do submit para dar refresh em toda pagina
+    eventoDoForm.preventDefault();
+    this.emails.push(this.email);
+
+    // criando uma nova instancia e limpando o objeto
+    this.limparEmail();
+
+    this.toogleNewEmailForm();
+  }
+
+  limparEmail() {
+    this.email = {
+      assunto : '',
+      destinatario : '',
+      conteudo : ''
+    }
+  }
   
   toogleNewEmailForm() {
     this._isNewEmailFormOpen = !this._isNewEmailFormOpen;
