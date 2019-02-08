ada2898f64cc1f91e73bc36f5374ab8efec23505
Cleberson <pauluci.cleberson@gmail.com>
2019-02-07 22:52:46 -0200

    Adicionado comportamento para diretivas, adicionando o id pelo placeholder

--- src/app/components/cmail-form-group/cmail-form-group.component.ts
+++ src/app/components/cmail-form-group/cmail-form-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'cmail-form-group',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CmailFormGroupComponent implements OnInit {
 
-  constructor() { }
+  elemento: ElementRef;
+  valorDaLabel: string;
 
+  // injecao de dependecia do angular
+  constructor(elemento: ElementRef) {
+    // console.log('constructor');
+    this.elemento = elemento;
+   }
+
+   // roda depois que o elemento foi criado na tela
   ngOnInit() {
+    const elementoInput = this.elemento.nativeElement.querySelector('input');
+
+    // recupera o valor passado pela label para injetar no html pelo one way data binding
+    this.valorDaLabel = elementoInput.getAttribute('placeholder');
+    elementoInput.setAttribute('placeholder', ' ');// tem que remover pois ele fica no campo input
   }
 
 }
