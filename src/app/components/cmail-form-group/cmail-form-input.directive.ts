580551c31aa298e0169909dd25a4d9749d8ab588
Cleberson <pauluci.cleberson@gmail.com>
2019-02-07 22:52:58 -0200

    Adicionado comportamento para diretivas, adicionando o id pelo placeholder

--- src/app/components/cmail-form-group/cmail-form-input.directive.ts
+++ src/app/components/cmail-form-group/cmail-form-input.directive.ts
@@ -9,6 +9,7 @@ export class CmailFormInpuDirective {
         // Injecao de dependencia (nao sei criar. o angular cria p nos)
         console.log("Diretiva sendo chamada!");
         const elemento = elementoHtml.nativeElement;
+        elemento.id = elemento.getAttribute('placeholder').replace(/\a/, '').toLowerCase();
         elemento.classList.add('mdl-textfield__input');
     }
 }
